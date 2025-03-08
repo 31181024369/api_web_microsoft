c46d0f23993753e0a7ef49f84668d8781a8dfe06
Nguyen Anh Toi <toidxbp02@gmail.com>
2025-03-08 11:03:21 +0700

    main

--- app/Models/Gift.php
+++ app/Models/Gift.php
@@ -2,20 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Gift extends Model
 {
-    use HasFactory;
-
-    protected $primaryKey = 'id';
-
     protected $fillable = [
         'title',
         'description',
         'reward_point',
         'picture',
-        'display',
+        'display'
+    ];
+
+    protected $casts = [
+        'display' => 'boolean',
+        'reward_point' => 'integer'
     ];
 }
