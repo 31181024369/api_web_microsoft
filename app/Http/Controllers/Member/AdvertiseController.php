7c69b8d23bb8fa825bc214fcaa9c788184f04f74
Nguyen Anh Toi <toidxbp02@gmail.com>
2025-03-09 12:30:22 +0700

    12:30

--- app/Http/Controllers/Member/AdvertiseController.php
+++ app/Http/Controllers/Member/AdvertiseController.php
@@ -10,27 +10,35 @@ class AdvertiseController extends Controller
 {
     public function showAdvertise(Request $request){
         try{
-            $advertise=Advertise::orderBy('id','asc')->where('display',1)->get();
-            $adpos=Adpos::get();
-            $data=[];
-            foreach($advertise as $item){
-                $adpos=Adpos::where('id_pos',$item->id_pos)->first();
-                $data[]=[
-                    'id'=>$item->id,
-                    'title'=>$item->title,
-                    'picture'=>$item->picture,
-                    'width'=>$item->width,
-                    'height'=>$item->height,
-                    'link'=>$item->link,
-                    'description'=>$item->description,
-                    'display'=>$item->display,
-                    'ad_pos'=>$adpos
-                ];
+            // $advertise=Advertise::where('display',1)->get()->groupBy('id_pos');
 
-            }
+
+            $advertise = Advertise::where('display', 1)
+            ->get()
+            ->groupBy(function ($item) {
+                return $item->Adpos ? $item->Adpos->name : $item->Adpos->title;
+            });
+            // return  $advertise;
+            // $adpos=Adpos::get();
+            // $data=[];
+            // foreach($advertise as $item){
+            //     $adpos=Adpos::where('id_pos',$item->id_pos)->first();
+            //     $data[]=[
+            //         'id'=>$item->id,
+            //         'title'=>$item->title,
+            //         'picture'=>$item->picture,
+            //         'width'=>$item->width,
+            //         'height'=>$item->height,
+            //         'link'=>$item->link,
+            //         'description'=>$item->description,
+            //         'display'=>$item->display,
+            //         'ad_pos'=>$adpos
+            //     ];
+
+            // }
             return response()->json([
-                'statis'=>true,
-                'data'=>$data
+                'status'=>true,
+                'data'=> $advertise
             ]);
         }catch (\Exception $e) {
             return response()->json([
