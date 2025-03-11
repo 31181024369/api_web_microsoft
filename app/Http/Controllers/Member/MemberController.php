8dbeb462e7d4f686b5dfcf81488c2b55d0cb977d
Nguyen Anh Toi <toidxbp02@gmail.com>
2025-03-11 16:53:05 +0700

    main

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -74,23 +74,23 @@ class MemberController extends Controller
                 $data = [
                     'subject' => ' Đăng Ký Tài Khoản Thành Công – Vui Lòng Chờ Duyệt',
                     'body' => '
-                    Kính gửi '. $member -> full_name .'<br>
+                    Kính gửi ' . $member->full_name . '<br>
                     Chúng tôi xin thông báo rằng bạn đã đăng ký tài khoản thành công trên Microsoft.
                      Tuy nhiên, tài khoản của bạn cần được xét duyệt trước khi có thể sử dụng.<br>
                     Dưới đây là thông tin tài khoản của Quý Khách:<br>
-                    Tên đăng nhập: '.$member -> username.'<br>
-                    Tên doanh nghiệp:'. $member ->nameCompany.'<br>
-                    Mã số thuế:'.$member -> tax.'<br>
-                    Thời gian đăng kí: '.$date.'<br>
+                    Tên đăng nhập: ' . $member->username . '<br>
+                    Tên doanh nghiệp:' . $member->nameCompany . '<br>
+                    Mã số thuế:' . $member->tax . '<br>
+                    Thời gian đăng kí: ' . $date . '<br>
                     Chúng tôi sẽ xem xét và duyệt tài khoản của bạn trong thời gian sớm nhất. Bạn sẽ nhận được thông báo ngay khi
                     tài khoản được kích hoạt.<br>
                     Nếu bạn có bất kỳ câu hỏi nào, vui lòng liên hệ với bộ phận hỗ trợ của chúng tôi.
                     Trân trọng.<br>
                     Đội ngũ Microsoft<br>'
                 ];
-                $to=$email;
+                $to = $email;
                 Mail::to($to)
-                ->send(new Notification($data));
+                    ->send(new Notification($data));
 
 
                 return response()->json([
@@ -136,8 +136,7 @@ class MemberController extends Controller
             }
 
             if ($member && $abbreviation == "$" && Hash::check($request->password, $member->password)) {
-                if($member->m_status == 0)
-                {
+                if ($member->m_status == 0) {
                     return response()->json([
                         'status' => false,
                         'success' => 'notApproved',
@@ -148,9 +147,17 @@ class MemberController extends Controller
                 $success = $member->createToken('Member')->accessToken;
 
                 $filteredMember = $member->makeHidden([
-                    'password', 'created_at', 'updated_at', 'number_passes',
-                    'm_status', 'city_province', 'ward', 'district',
-                    'provider', 'password_token', 'address'
+                    'password',
+                    'created_at',
+                    'updated_at',
+                    'number_passes',
+                    'm_status',
+                    'city_province',
+                    'ward',
+                    'district',
+                    'provider',
+                    'password_token',
+                    'address'
                 ]);
 
                 return response()->json([
